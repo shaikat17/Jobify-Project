44b555e3158c8f369c7931ca9954ef2c3f2986f5
shaikat17 <shaikatpal56@gmail.com>
2024-09-04 21:43:27 +0600

    Working on page routing. Installed react router dom.

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,18 +1,19 @@
-import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
           <h1>Job <span>tracking</span> app</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit itaque necessitatibus, distinctio facere neque voluptates facilis deleniti ullam adipisci error?</p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">Login/Register</Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Landing from "./Landing";
+import ErrorPage from "./ErrorPage";
+import Register from "./Register";
+import Dashboard from "./Dashboard";
+
+export { Landing, ErrorPage, Register, Dashboard }
