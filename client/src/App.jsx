3542f1f9b794bed5bf4a4af3d759d79aa0f8efbd
shaikat17 <shaikatpal56@gmail.com>
2024-09-04 14:50:11 +0600

    normalize css added, landing page completed.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
+import Landing from './pages/Landing'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Landing />
     </>
   )
 }
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,24 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>Job <span>tracking</span> app</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit itaque necessitatibus, distinctio facere neque voluptates facilis deleniti ullam adipisci error?</p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  )
+}
+
+
+export default Landing
