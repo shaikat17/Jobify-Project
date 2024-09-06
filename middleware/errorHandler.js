feeb6962ca9dc2dca429e9908a2cf7aab3f12beb
shaikat17 <shaikatpal56@gmail.com>
2024-09-06 14:00:44 +0600

    used bcryptjs to hash user password.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,9 @@
 import { StatusCodes } from "http-status-codes"
 
-const errorHandlerMiddleware = (err, req, res, next) => {
+const errorHandlerMiddleware = (err, req, res) => {
+    console.log("🚀 ~ errorHandlerMiddleware ~ err:", err)
+    
+
     const defaultError = {
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg: err.message || 'Something went wrong, try again later.',
