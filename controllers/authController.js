39132d504a5467ae817288eb47778057b02b7a06
shaikat17 <shaikatpal56@gmail.com>
2024-09-06 13:05:35 +0600

    Error handle works done.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,33 @@
-const register = (req, res) => {
-    res.send('register user')
-}
+import {BadRequestError} from "../errors/index.js";
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+
+
+const register = async (req, res, next) => {
+  const { name, email, password } = req.body
+  
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide all values.')
+  }
+
+  const userAlreadyExits = await User.findOne({ email })
+  
+  if (userAlreadyExits) {
+    throw new BadRequestError('Email already in use.')
+  }
+
+    const user = await User.create({name, email, password});
+    res.status(StatusCodes.OK).json({ user });
+  
+};
 
 const login = (req, res) => {
-    res.send('login user')
-}
+  res.send("login user");
+};
 
 const updateUser = (req, res) => {
-    res.send('update user')
-}
+  res.send("update user");
+};
 
-export { register, login, updateUser }
+
+export { register, login, updateUser };
---
+++ errors/index.js
@@ -0,0 +1,4 @@
+import BadRequestError from "./badRequestError.js";
+import NotFoundError from "./notFoundError.js";
+
+export {BadRequestError, NotFoundError}
