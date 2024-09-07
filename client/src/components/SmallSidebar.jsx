78157a0ac3de4a805a2ad8cdb71d30ed71e13985
shaikat17 <shaikatpal56@gmail.com>
2024-09-07 16:09:12 +0600

    design and functionality added for small sidebar

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,8 +1,31 @@
 import Wrapper from "../assets/wrappers/SmallSidebar"
+import { FaTimes } from "react-icons/fa"
+import { NavLink } from 'react-router-dom'
+import Logo from "./Logo"
+import { useAppContext } from "../context/AppContext"
+import links from "../utils/links"
 
 const SmallSidebar = () => {
+  const { toggleSideBar, showSidebar } = useAppContext()
   return (
-    <Wrapper><h4>Small sidebar</h4></Wrapper>
+    <Wrapper>
+      <div className={showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"}>
+        <div className="content">
+          <button className="close-btn" onClick={toggleSideBar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link
+              return <NavLink to={path} key={id} onClick={toggleSideBar} className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}><span className="icon">{icon}</span>{text}</NavLink>
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 export default SmallSidebar
